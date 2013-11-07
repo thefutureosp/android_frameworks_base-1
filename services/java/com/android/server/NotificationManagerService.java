617bdbfcab992a8ff2af27250b18502686703aea
Der-Schubi <schubi@erlangen.ccc.de>
2013-11-07 21:47:17 -0200

    Option to enable Notification Light when screen is on (1/2) This makes possible to enable the Notification LED even when the screen is on
    Change-Id: Id939e19563eae1f530f1a2f04aa706dbc9c12567

--- services/java/com/android/server/NotificationManagerService.java
+++ services/java/com/android/server/NotificationManagerService.java
@@ -1313,6 +1313,9 @@ public class NotificationManagerService extends INotificationManager.Stub
             boolean queryRemove = false;
             boolean packageChanged = false;
             boolean cancelNotifications = true;
+
+	    boolean ScreenOnNotificationLed = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.SCREEN_ON_NOTIFICATION_LED, 1) == 1; 
             
             if (action.equals(Intent.ACTION_PACKAGE_ADDED)
                     || (queryRemove=action.equals(Intent.ACTION_PACKAGE_REMOVED))
@@ -1405,7 +1408,7 @@ public class NotificationManagerService extends INotificationManager.Stub
                 }
             } else if (action.equals(Intent.ACTION_USER_PRESENT)) {
                 // turn off LED when user passes through lock screen
-                if (!mDreaming) {
+                if (!mDreaming && !ScreenOnNotificationLed) { 
                     mNotificationLight.turnOff();
                 }
             } else if (action.equals(Intent.ACTION_USER_SWITCHED)) {
@@ -2556,6 +2559,8 @@ public class NotificationManagerService extends INotificationManager.Stub
 
     // lock on mNotificationList
     private void updateLightsLocked() {
+        boolean ScreenOnNotificationLed = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.SCREEN_ON_NOTIFICATION_LED, 1) == 1;
         // handle notification lights
         if (mLedNotification == null) {
             // use most recent light with highest score
@@ -2570,8 +2575,8 @@ public class NotificationManagerService extends INotificationManager.Stub
 
         // Don't flash while we are in a call, screen is on or we are
         // in quiet hours with light dimmed
-        if (mLedNotification == null || mInCall
-                || (mScreenOn && !mDreaming) || (inQuietHours() && mQuietHoursDim)) {
+        if (mLedNotification == null || mInCall || (mScreenOn && !ScreenOnNotificationLed && !mDreaming) 
+            		|| (inQuietHours() && mQuietHoursDim)) { 
             mNotificationLight.turnOff();
         } else if (mNotificationPulseEnabled) {
             final Notification ledno = mLedNotification.sbn.getNotification();
